640493ddbb29a31ad0707f0275afea4570dfad7f
Adrian Lugo <misteralugo@gmail.com>
2021-10-25 18:42:08 -0400

    number buttons with hover

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+const blockSubjects = document.querySelectorAll(".block-subject")
+
+blockSubjects.forEach(addNumbers)
+
+function addNumbers(item) {
+    for (let i = 0; i < 12; i++) {
+        const btn = document.createElement("button")
+        item.appendChild(btn)
+        btn.textContent = i + 1
+    }
+}
